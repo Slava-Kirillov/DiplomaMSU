cad2b656f51ee0f0c7eac7891d8fd47e3d8db14b
vkirillov <Viacheslav.Kirillov@center2m.ru>
2019-09-27 10:54:47 +0300

    read file data

--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+#include <bits/types/FILE.h>
 
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
+    FILE *file = fopen("../resource/data/test_file", "wb");
+    if (file == NULL) {
+        printf("File reading error\n");
+    }
+
     return 0;
 }
